575dd37a1f0d964a531875627ede4ba09953fad0
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-03-28 16:17:15 -0700

    working on Utility

--- src/utils.py
+++ src/utils.py
@@ -10,16 +10,20 @@ from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
+        logging.info(f"Saving the object on file path: {file_path}")
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path,exist_ok=True)
         with open(file_path,'wb') as file_obj:
             dill.dump(obj,file_obj)
+        logging.info(f"File object saved successfully")
     except Exception as e:
         raise CustomException(e,sys)
 
 def load_obj(file_path):
     try:
+        logging.info(f"Loading file object from the given file path: {file_path}")
         with open(file_path,'rb') as file_obj:
+            logging.info("File object loaded successfully")
             return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
