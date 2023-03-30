27bdcaa5cc92e27f6c6d367e48a6017ee86e8baf
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-03-29 21:23:00 -0700

    data ingestion working

--- src/model_components/data_ingestion.py
+++ src/model_components/data_ingestion.py
@@ -23,9 +23,9 @@ class DataIngestion():
             logging.info(f"Data is being consumed from: {file_path}")
             os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path),exist_ok=True)
             df.to_csv(self.data_ingestion_config.raw_data_path,index=False,header=True)
+            batch_size = 60
             logging.info("Beginning train test data split")
-            #train_set,test_set = train_test_split(df,test_size=0.2)
-            train_set,test_set = self.test_train_stock_split(df)
+            train_set,test_set = self.test_train_stock_split(df,batch_size)
             logging.info("Saving different files")
             train_set.to_csv(self.data_ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.data_ingestion_config.test_data_path,index=False,header=True)
@@ -37,8 +37,7 @@ class DataIngestion():
         except Exception as e:
             raise CustomException(e,sys)
         
-    def test_train_stock_split(self,raw_data):
-        batch_size = 60
+    def test_train_stock_split(self,raw_data,batch_size):
         logging.info(f"Using a btach size of {batch_size} to split the data into test and train data")
         try:
             train_data = raw_data[:-batch_size]
