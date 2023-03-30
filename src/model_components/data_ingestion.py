2585173da0ee2650a47041d5966c8893723d1de8
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-03-29 19:27:53 -0700

    working on Ingestion

--- src/model_components/data_ingestion.py
+++ src/model_components/data_ingestion.py
@@ -5,4 +5,24 @@ from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+@dataclass
+class DataIngestionConfig():
+    train_data_path: str = os.join.path('artifacts','train.csv')
+    test_data_path: str = os.join.path('artifacts','test.csv')
+    raw_data_path: str = os.join.path('artifacts','data.csv')
+
+class DataIngestion():
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionConfig()
+    
+    def initate_data_ingestion(self):
+        logging.info("Beginning data Ingestion")
+        try:
+            pass
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+if __name__ == "__main__":
+    pass
 
