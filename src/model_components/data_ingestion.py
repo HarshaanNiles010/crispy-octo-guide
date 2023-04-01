8a8df1795c5e6c5ffc45aea687797ec81f035ac5
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-03-31 19:28:12 -0700

    working on transformation

--- src/model_components/data_ingestion.py
+++ src/model_components/data_ingestion.py
@@ -5,6 +5,10 @@ from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.model_components.data_transformation import DataTransformationConfig
+from src.model_components.data_transformation import DataTransformation
+
 @dataclass
 class DataIngestionConfig():
     train_data_path: str = os.path.join('artifacts','train.csv')
@@ -52,5 +56,6 @@ class DataIngestion():
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initate_data_ingestion()
-
+    train_data,test_data = obj.initate_data_ingestion()
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
--- src/model_components/data_transformation.py
+++ src/model_components/data_transformation.py
@@ -1,9 +1,10 @@
 import os
 import sys
 import pandas as pd
+import numpy as np
 from src.exception import CustomException
 from src.logger import logging
-from utils import save_object
+from src.utils import save_object
 from dataclasses import dataclass
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
@@ -26,7 +27,6 @@ class DataTransformation():
                 "Open",
                 "High",
                 "Low",
-                "Close",
                 "Adj Close",
                 "Volume"
             ]
@@ -48,10 +48,50 @@ class DataTransformation():
             raise CustomException(e,sys)
     
     
-    def initiate_data_transformation(self):
+    def initiate_data_transformation(self,train_path:str,test_path:str):
         logging.info("Initiating data transformation")
         try:
-            pass
+            logging.info("Reading train and test data")
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            logging.info("Read train and test data completed")
+            logging.info("Obtaining preprocessing object")
+            preprocessing_obj=self.get_data_transformer_object()
+            target_column_name="Close"
+            numerical_column_name = [
+                "Date",
+                "Open",
+                "High",
+                "Low",
+                "Close",
+                "Adj Close",
+                "Volume"
+            ]
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+            
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+            
+            logging.info("Applying preprocessing object on training dataframe and testing dataframe.")
+            
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            logging.info(f"Saved preprocessing object.")
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
         except Exception as e:
             raise CustomException(e,sys)
 
