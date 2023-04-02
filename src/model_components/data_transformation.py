4a6714e92c9ab84959789f0bd3e71d60fc75c057
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-04-02 00:56:45 -0700

    Transformation somewhat working

--- src/model_components/data_transformation.py
+++ src/model_components/data_transformation.py
@@ -52,66 +52,6 @@ class DataTransformation():
     def initiate_data_transformation(self,train_path:str,test_path:str):
         logging.info("Initiating data transformation")
         try:
-            """ logging.info("Reading train and test data")
-            train_df = pd.read_csv(train_path)
-            test_df = pd.read_csv(test_path)
-            logging.info("Read train and test data completed")
-            logging.info("Obtaining preprocessing object")
-            preprocessing_obj=self.get_data_transformer_object()
-            target_column_name="Close"
-            numerical_column_name = [
-                "Date",
-                "Open",
-                "High",
-                "Low",
-                "Close",
-                "Adj Close",
-                "Volume"
-            ]
-            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df=train_df[target_column_name]
-            
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df=test_df[target_column_name]
-            
-            logging.info("Applying preprocessing object on training dataframe and testing dataframe.")
-            
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
-            train_arr = np.c_[
-                input_feature_train_arr, np.array(target_feature_train_df)
-            ]
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
-            logging.info(f"Saved preprocessing object.")
-            save_object(
-                file_path=self.data_transformation_config.preprocessor_obj_file_path,
-                obj=preprocessing_obj
-            )
-            return (
-                train_arr,
-                test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path,
-            ) """
-            """ logging.info("Reading train and test data")
-            train_df = pd.read_csv(train_path)
-            test_df = pd.read_csv(test_path)
-            logging.info("Read train and test data completed")
-            new_scalar = MinMaxScaler(feature_range=(0,1))
-            scaled_data = new_scalar.fit_transform(train_df['Close'].values.reshape(-1,1))
-            prediction_days = 60
-            x_train = []
-            y_train = []
-            for i in range(prediction_days,len(scaled_data)):
-                x_train.append(scaled_data[i-prediction_days:i,0])
-                y_train.append(scaled_data[i,0])
-            x_train, y_train = np.array(x_train), np.array(y_train)
-            x_train = np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
-            return(
-                x_train,
-                y_train,
-                self.data_transformation_config.preprocessor_obj_file_path
-            ) """
             logging.info("Reading train and test data")
             train_df = pd.read_csv(train_path)
             test_df = pd.read_csv(test_path)
@@ -120,6 +60,7 @@ class DataTransformation():
             preprocessing_obj = self.get_data_transformer_object()
             logging.info("Preprocessor object obtained")
             target_column_name="Close"
+            logging.info(f"The target column chosen is: {target_column_name}")
             input_feature_train_df = train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df = train_df[target_column_name]
             input_feature_test_df = test_df.drop(columns=[target_column_name],axis=1)
