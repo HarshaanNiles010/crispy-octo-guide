c2805140b401b3c330fbe47f7b8903a49e4e278c
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-04-03 00:37:38 -0700

    Working on model trainer

--- src/model_components/model_trainer.py
+++ src/model_components/model_trainer.py
@@ -6,4 +6,20 @@ from src.exception import CustomException
 from src.logger import logging
 from keras.models import Sequential
 from keras.layers import Dense,Dropout,LSTM
+from dataclasses import dataclass
 
+from src.utils import save_object
+
+@dataclass
+class ModelTrainerConfig():
+    trainer_model_file_path:str = os.path.join('artifacts','model.pkl')
+
+class ModelTrainer():
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+    
+    def initate_model_trainer(self,train_array,test_array):
+        try:
+            pass
+        except Exception as e:
+            raise CustomException(e,sys)
