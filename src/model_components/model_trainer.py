1d5eb30755cbe3d2d295645804fbff6e6889c107
HarshaanNiles010 <harshaanniles010@gmail.com>
2023-04-03 00:33:02 -0700

    Working on model trainer

--- src/model_components/model_trainer.py
+++ src/model_components/model_trainer.py
@@ -4,3 +4,6 @@ import pandas as pd
 import numpy as np 
 from src.exception import CustomException
 from src.logger import logging
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,LSTM
+
